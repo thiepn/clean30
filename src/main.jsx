4455354df6c3423cc8e1abe7aa3a59332edd154a
thiepn <junsosmart@gmail.com>
2026-08-04 14:47:03 +0200

    Load universal Phase 1 styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import "./styles.css";
+import "./styles/universal-phase1.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
