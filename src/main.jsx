52afe6bb0a0ca0321d2c8aba335b174c0b1b0490
thiepn <junsosmart@gmail.com>
2026-08-04 15:35:28 +0200

    Load Phase 2 Today styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import "./styles.css";
 import "./styles/universal-phase1.css";
+import "./styles/universal-phase2.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
