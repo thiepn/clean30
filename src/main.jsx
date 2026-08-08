95a23636b2ac319bd2b0acd9e63efa56df4b28e5
thiepn <junsosmart@gmail.com>
2026-08-08 03:44:40 +0200

    Load Phase 6 polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./styles/universal-phase2.css";
 import "./styles/universal-phase3.css";
 import "./styles/universal-phase4.css";
 import "./styles/universal-phase5.css";
+import "./styles/universal-phase6.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
